a6b0810d91609449b74190b28210cff935890c76
unknown <vitorjao56@gmail.com>
2020-07-09 20:17:32 -0300

    adiciona index e details da pagina de admin

--- controller/admin.js
+++ controller/admin.js
@@ -2,5 +2,10 @@ const fs = require("fs");
 const data = require("../data.json");
 
 exports.index = (req, res) => {
-  return res.render("index");
+  return res.render("./admin/index", { datas: data.recipes });
+};
+
+exports.detail = (req, res) => {
+  const { id } = req.params;
+  return res.render("./admin/detail", { data: data.recipes[id] });
 };
--- controller/routes.js
+++ controller/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = express.Router();
 const data = require("../data");
 const user = require("./user");
+const admin = require("./admin");
 
 // USER AREA
 
@@ -12,5 +13,11 @@ routes.get("/receitas", user.recipes);
 routes.get("/about", user.about);
 
 routes.get("/recipes/:id", user.detail);
+
+// ADMIN AREA
+
+routes.get("/admin", admin.index);
+
+routes.get("/admin/recipes/:id", admin.detail);
 
 module.exports = routes;
