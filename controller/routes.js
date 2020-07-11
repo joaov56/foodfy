52347398371bb5a610bd8f2758f8102e78ee5e94
unknown <vitorjao56@gmail.com>
2020-07-10 23:39:38 -0300

    adiciona funções de administrador de criar e editar

--- controller/routes.js
+++ controller/routes.js
@@ -20,4 +20,14 @@ routes.get("/admin", admin.index);
 
 routes.get("/admin/recipes/:id", admin.detail);
 
+routes.get("/admin/recipes/:id/edit", admin.edit);
+
+routes.get("/admin/create", admin.create);
+
+routes.put("/admin/recipes", admin.put);
+
+routes.delete("/admin/recipes", admin.delete);
+
+routes.post("/admin/recipes", admin.post);
+
 module.exports = routes;
